af27057b2f6e21b56f9b391cbd4f1a6977fc1634
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-26 22:15:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project000
+
+Developed with Unreal Engine 4
---
+++ Source/Project000/Project000GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project000GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT000_API AProject000GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
